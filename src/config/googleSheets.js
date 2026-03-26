6337aaa98828d7008f0b3e9226ebe9b76e0c9c64
Moez <moez.benjemiaa@etudiant-fst.utm.tn>
2026-03-26 22:46:04 +0100

    Initial commit - Quantum AI Trading Platform

--- src/config/googleSheets.js
+++ src/config/googleSheets.js
@@ -1,4 +1,4 @@
 // Replace with your actual Google Apps Script URL
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzfUQCIh9c86hbgncQn2vBH5XebiPkE9yVYJWabDZX7aRoIKtUwXC5riGh-6mTVGiFf/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxxO7OBcFQl8a5e61RtsWOsenM4Q1tau66pYRjJOAqf1extOGhYfJSM1BtSjnJvh_eU/exec";
 
 export default GOOGLE_SCRIPT_URL;
